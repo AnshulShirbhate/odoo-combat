35309d4e2008d1e97e0e0e288d5123389ebe5e1b
Shluch <adityaalat@hotmail.com>
2024-06-29 15:36:27 +0530

    Forntend2

--- renting-furniture/src/App.js
+++ renting-furniture/src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import Home from "./components/Home";
+import Products_list from "./components/Products_list";
+import Article from "./components/Article";
 
 function App() {
-  return <Home />;
+  return <Article/>;
 }
 
 export default App;
