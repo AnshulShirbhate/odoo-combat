81e0662ebbc10017bdd4f9e381aee2a0d6b188e4
VaidehiThool <118549564+VaidehiThool@users.noreply.github.com>
2024-06-29 15:30:24 +0530

    front

--- renting-furniture/src/App.js
+++ renting-furniture/src/App.js
@@ -1,9 +1,18 @@
-import logo from "./logo.svg";
+
 import "./App.css";
-import Home from "./components/Home";
+import Category from "./components/Category";
+import BlogGrid from "./components/BlogGrid";
+
+
 
 function App() {
-  return <Home />;
+  return(
+    <>
+      <Category/>;
+      <BlogGrid/>
+    </>
+  )
+
 }
 
 export default App;
